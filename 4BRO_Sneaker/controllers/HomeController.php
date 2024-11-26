c6a4656116099734e56eb563b174cd320eec0992
Xuan270305 <xuanntph50681@gmail.com>
2024-11-26 17:35:22 +0700

    đk dn

--- 4BRO_Sneaker/controllers/HomeController.php
+++ 4BRO_Sneaker/controllers/HomeController.php
@@ -4,11 +4,13 @@ class HomeController
 {
     public $modelSanPham;
     public $modelDanhMuc;
-
+    public $modelTaiKhoan;
     public function __construct()
     {
         $this->modelSanPham = new SanPham();
+        $this->modelTaiKhoan = new TaiKhoan();
     }
+    
     public function home()
     {
         $listSanPham = $this->modelSanPham -> getAllSanPham();
@@ -34,5 +36,75 @@ $listSanPhamvaDanhMuc=$this->modelSanPham->getlistSanPhamDanhMuc($sanpham['danh_
         }
     }
 
-   
+    public function formLogin(){
+        require_once './views/auth/formLogin.php';
+
+        $this->deleteSessionError();
+
+    }
+    public function formRegister(){
+        require_once './views/auth/formDk.php';
+
+        $this->deleteSessionError();
+
+    }
+
+
+    public function postLogin(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //lay email va passs
+
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $user = $this->modelTaiKhoan->checkLogin($email, $password);
+
+            if($user ==  $email) {
+                $_SESSION['user_client'] = $user;
+                header("Location: " . BASE_URL);
+                exit();
+            } else {
+                $_SESSION['error'] = $user;
+
+                // var_dump($_SESSION['error']);die; 
+
+                $_SESSION['flash'] = true;
+
+                header("Location: " . BASE_URL . '?act=login');
+                exit();
+            }
+        }
+    }
+
+    public function postRegister() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Lấy thông tin từ form đăng ký
+            $fullname = $_POST['ho_ten'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $confirmPassword = $_POST['confirm_password'];
+    
+            // Kiểm tra mật khẩu và xác nhận mật khẩu có khớp hay không
+            if ($password !== $confirmPassword) {
+                $_SESSION['register_error'] = "Mật khẩu và xác nhận mật khẩu không khớp";
+                $_SESSION['flash'] = true;
+                header("Location: " . BASE_URL . '?act=register');
+                exit();
+            }
+    
+            // Đăng ký tài khoản mới
+            $result = $this->modelTaiKhoan->register($fullname, $email, $password);
+    
+            if ($result === "Đăng ký thành công") {
+                $_SESSION['success'] = $result;
+                header("Location: " . BASE_URL . '?act=login');
+                exit();
+            } else {
+                $_SESSION['register_error'] = $result;
+                $_SESSION['flash'] = true;
+                header("Location: " . BASE_URL . '?act=register');
+                exit();
+            }
+        }
+    }
 }
